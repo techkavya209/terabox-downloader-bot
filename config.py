c913a1914daa9b4ed441a83a8e50dc39f656c049
techkavya209 <ohrbots@gmail.com>
2024-11-30 14:37:47 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,4 @@
-API_ID = 28200856  # api id
-API_HASH = "30e0567e3c97b0ea8e8d3fe50b2c2442"  # api hash
-
-BOT_TOKEN = "7925232821:AAGTM6Ao2tswlQemSZXhGr2GH-8vSZ8mNIY"  # bot token
+mNIY"  # bot token
 
 
 # REDIS
@@ -10,13 +7,7 @@ PORT = 6379  # redis port
 PASSWORD = ""  # redis password
 
 PRIVATE_CHAT_ID =-1002377512253# CHAT WHERE YOU WANT TO STORE VIDEOS
-# COOKIE FOR AUTHENTICATION (get from chrome dev tools) ex: "PANWEB=1; csrfToken=;
-COOKIE = ""
-ADMINS = [7154442141]
-
-
-BOT_USERNAME = "teraboxdown_bot"
-
+# COOKIE FOR AUTHENTICATION (get from chrome dev 5
 # Force user to join this channel. (make sure you have promoted the bot on this chat.)
 FORCE_LINK = "@jfnddndnek"
 
